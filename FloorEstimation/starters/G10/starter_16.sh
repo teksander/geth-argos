b4ebc6d6a4a31da69dc7b1d48bf0f9ef99b3e220
Ubuntu <ubuntu@ip-172-31-21-126.us-east-2.compute.internal>
2021-12-08 23:09:38 +0000

    added 16 starter

--- FloorEstimation/starters/G10/starter_16.sh
+++ FloorEstimation/starters/G10/starter_16.sh
@@ -8,10 +8,10 @@ export LOOPFUNCTION="floor_estimation_no_convergence_loop.py"
 export LENGTH=36000
 export NUMROBOTS=16
 export robots=16
-export num_byzantines=2
+export num_byzantines=4
 export byzantine_swarm_style=1
-export FLOORFILE=22.png
-export ARENADIM=1.10
-export ARENADIMH=0.55
+export FLOORFILE=31.png
+export ARENADIM=1.55
+export ARENADIMH=0.775
 
 source run_experiment.sh
