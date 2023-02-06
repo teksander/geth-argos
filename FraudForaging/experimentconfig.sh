3414f90a759b8b917cf4859e6a5aa59d8b0af0da
teksander <alexandre.melo.pacheco@gmail.com>
2023-02-07 00:59:26 +0100

    new branch for RGB consensus

--- FraudForaging/experimentconfig.sh
+++ FraudForaging/experimentconfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash  
 
 # [PATHS]
-export MAINFOLDER="/home/hzhao/blockchain_robots/geth-argos"
+export MAINFOLDER="$HOME/geth-argos"
 export DOCKERFOLDER="$MAINFOLDER/argos-blockchain"
 export ARGOSFOLDER="$MAINFOLDER/argos-python"
 export EXPERIMENTFOLDER="$MAINFOLDER/FraudForaging"
